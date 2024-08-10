a14d905ecd7f3bf9c0b10b91e122ff286c32c05f
Leander <leander.weinmann@gmx.net>
2024-08-11 00:14:42 +0200

    Stats + UI

--- scenes/sc_coll_coin.gd
+++ scenes/sc_coll_coin.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-@onready var game_manager = %"Game Manager"
+@onready var coin_score = %"Coin Score"
 
 func _on_body_entered(body):
 	if (body.name == "Player"):
 		queue_free()
-		game_manager.add_points()
+		coin_score.add_points()

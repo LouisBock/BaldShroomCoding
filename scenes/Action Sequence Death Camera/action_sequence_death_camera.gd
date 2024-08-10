964e69f429228ff7ffff042e9c8ce594dccb5d3c
Louis Bock <louislb04@gmail.com>
2024-08-11 00:20:32 +0200

    action sequence camera

--- scenes/Action Sequence Death Camera/action_sequence_death_camera.gd
+++ scenes/Action Sequence Death Camera/action_sequence_death_camera.gd
@@ -4,15 +4,7 @@ extends Node2D
 @onready var rigid_body_2d = $RigidBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _on_rigid_body_2d_body_entered(body):
-	
-	pass # Replace with function body.
+	if (body.name == "Player"):
+		print("YOU DIED")
+

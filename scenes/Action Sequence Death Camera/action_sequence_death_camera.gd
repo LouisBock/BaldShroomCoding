e960d36d9fbb763048ab944e2d7d9e5bdf21ad48
Louis Bock <louislb04@gmail.com>
2024-08-11 03:05:01 +0200

    action sequence

--- scenes/Action Sequence Death Camera/action_sequence_death_camera.gd
+++ scenes/Action Sequence Death Camera/action_sequence_death_camera.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
-@onready var rigid_body_2d = $RigidBody2D
+@export var player: Node2D 
+@onready var area = $Area2D
+@onready var cam = $Area2D/Camera2D
+var speed = 200
 
-func _on_rigid_body_2d_body_entered(body):
+func _ready():
+	cam.position.x = cam.get_viewport().size[0]*0.5
+
+func _process(delta):
+	area.move_local_x(delta * speed)
+	area.position.y = player.position.y - cam.get_viewport().size[1]*0.5
+
+func _on_area_2d_body_entered(body):
+	print(body.name)
 	if (body.name == "Player"):
-		print("YOU DIED")
-
+		print("YOU DIED")# Replace with function body.

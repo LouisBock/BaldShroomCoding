d2911a115da0e78945857c291ec1928e9072dc54
Leander <leander.weinmann@gmx.net>
2024-08-12 10:15:53 +0200

    Aeri

--- scenes/sc_companion.gd
+++ scenes/sc_companion.gd
@@ -1,12 +1,67 @@
 extends Node
 
-@export var speed = 10
+# Wanted x pos where animation should play
+@export var x_left = 15
+@export var x_right = -30
+
+# Wanted y pos where animation should play
+@export var y_pos = -20
+
+@export var speed = 1
+
+var max_distance_diff = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$animAeri.play("idle")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	# Reset pos. to left, when player goes left
+	if Input.is_action_just_pressed("move_left"):
+		self.position.x = x_left
+		self.position.y = y_pos
+	
+	# Aeri pos. goes to the left with movement; to certain extend
+	if Input.is_action_pressed("move_left"):
+		self.position.x += speed
+		max_distance_diff += 1
+		if (max_distance_diff >= 50):
+			speed = 0
+	
+	# Reset pos. to right, when player goes right
+	if Input.is_action_just_pressed("move_right"):
+		self.position.x = x_right
+		self.position.y = y_pos
+	
+	# Aeri pos. goes to the right with movement; to certain extend
+	if Input.is_action_pressed("move_right"):
+		self.position.x -= speed
+		max_distance_diff += 1
+		if (max_distance_diff >= 50):
+			speed = 0
+	
+	if Input.is_action_just_released("move_left"):
+		max_distance_diff = 0
+		speed = 1
+		
+		move_back_left()
+	
+	if Input.is_action_just_released("move_right"):
+		max_distance_diff = 0
+		speed = 1
+		
+		move_back_right()
+	
+
+# Funktionen, um Aeri wieder auf die Ursprungsposition zurück zu führen
+# Hier actual path zurück implementieren
+func move_back_left():
+	if self.position.x != 15:
+		self.position.x = 15
+		
+
+func move_back_right():
+	if self.position.x != -30:
+		self.position.x = -30
+		
+
